cc6181e88c8fbc32ce1d57ac36a0834a1d2f42da
wellingtonfrancis95-byte <wellington.francis.95@hotmail.com>
2026-03-25 15:44:20 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'cnc-v37';
+const assets = [
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
