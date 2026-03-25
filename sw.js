295569a25b58bb072c7f7ee534d6691fd6a3f5e3
wellingtonfrancis95-byte <wellington.francis.95@hotmail.com>
2026-03-25 16:25:39 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -11,6 +11,14 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
